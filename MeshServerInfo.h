ba431b4fc8d91a7af1afbf7fddc7f82ad099ea77
Robert Maynard <robert.maynard@kitware.com>
2012-03-21 10:08:52 -0400

    Add in helper headers.
    The helper headers define common zeroMQ functions and define the ports
    that the components of the mesh server architecture connect too.

---
+++ MeshServerInfo.h
@@ -0,0 +1,20 @@
+/*=========================================================================
+  
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+  
+=========================================================================*/
+
+#ifndef __MeshServerInfo_h
+#define __MeshServerInfo_h
+
+//Define global information that the mesh server needs
+
+namespace meshserver {
+static const int BROKER_CLIENT_PORT = 5555;
+static const int BROKER_WORKER_PORT = 5556;
+static const int BROKER_STATUS_PORT = 5557;
+}
+
+#endif // __MeshServerInfo_
