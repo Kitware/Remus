11cfc20896ae1e597cbdac35d4c4575e17c9f1b3
Robert Maynard <robert.maynard@kitware.com>
2012-04-02 09:57:03 -0400

    Adding in requirement on boost thread so that workers can report crashes.

--- meshserver/Worker.h
+++ meshserver/Worker.h
@@ -11,6 +11,12 @@
 
 #include <zmq.hpp>
 #include <string>
+#include <set>
+
+#include <boost/thread/condition_variable.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread.hpp>
+#include <csignal>
 
 #include <meshserver/JobMessage.h>
 #include <meshserver/JobResponse.h>
@@ -29,6 +35,7 @@ class Worker
 public:
   //constuct a worker that can mesh a single type
   Worker(meshserver::MESH_TYPE mtype);
+  ~Worker();
 
   //gets back a job from the broker
   //this will lock the worker as it will wait on a job message
@@ -41,6 +48,12 @@ public:
   void returnMeshResults(const meshserver::common::JobResult& result);
 
 private:
+  //setups the signal handling for exceptions
+  //these allow us to register to a static variable that we should
+  //handle a crash.
+  void setupCrashHandling();
+  static void crashHandler(int value);
+
   //holds the type of mesh we support
   const meshserver::MESH_TYPE MeshType;
 
@@ -48,15 +61,87 @@ private:
 
   //this socket is used to talk to broker
   zmq::socket_t Broker;
+
+  //holds all the job ids this worker is processing
+  typedef std::set<boost::uuids::uuid>::const_iterator CJ_It;
+  std::set<boost::uuids::uuid> CurrentJobIds;
+
+
+  //this is the thread that handles notify the broker when we catch
+  //a signal that means we have to die
+  boost::thread* ExceptionHandlingThread;
+  void reportCrash(); //method the thread uses to report to broker
+  static int CaughtCrashSignal;
+  static boost::condition_variable CrashCond;
+  static boost::mutex CrashMutex;
 };
+
+
+int Worker::CaughtCrashSignal = 0;
+boost::condition_variable Worker::CrashCond;
+boost::mutex Worker::CrashMutex;
 
 //-----------------------------------------------------------------------------
 Worker::Worker(meshserver::MESH_TYPE mtype):
   MeshType(mtype),
   Context(1),
-  Broker(this->Context,ZMQ_DEALER)
+  Broker(this->Context,ZMQ_DEALER),
+  CurrentJobIds()
 {
   zmq::connectToSocket(this->Broker,meshserver::BROKER_WORKER_PORT);
+
+  //setup exception handling CTRL+C etc failures
+  this->setupCrashHandling();
+
+  //setup the thread that will report back to the broker
+  this->ExceptionHandlingThread = new boost::thread(&Worker::reportCrash,this);
+
+}
+
+//-----------------------------------------------------------------------------
+Worker::~Worker()
+{
+  delete this->ExceptionHandlingThread;
+}
+
+//-----------------------------------------------------------------------------
+void Worker::setupCrashHandling()
+{
+  struct sigaction action;
+  action.sa_handler = Worker::crashHandler;
+  action.sa_flags = 0;
+  sigemptyset(&action.sa_mask);
+  sigaction(SIGINT,&action,NULL);
+  sigaction(SIGTERM,&action,NULL);
+}
+
+//-----------------------------------------------------------------------------
+void Worker::crashHandler(int value)
+{
+  boost::lock_guard<boost::mutex> lock(Worker::CrashMutex);
+  Worker::CaughtCrashSignal = value;
+  Worker::CrashCond.notify_all();
+}
+
+//-----------------------------------------------------------------------------
+void Worker::reportCrash()
+{
+
+  boost::unique_lock<boost::mutex> lock(Worker::CrashMutex);
+  while(this->CaughtCrashSignal==0)
+    {
+    Worker::CrashCond.wait(lock);
+    }
+
+  for(CJ_It i=this->CurrentJobIds.begin();
+      i != this->CurrentJobIds.end(); ++i)
+    {
+    const meshserver::common::JobStatus js(*i, meshserver::FAILED);
+    const std::string msg(meshserver::to_string(js));
+    meshserver::JobMessage jm(this->MeshType,meshserver::MESH_STATUS,msg.data(),msg.size());
+    jm.send(this->Broker);
+    }
+  exit(this->CaughtCrashSignal);
 }
 
 //-----------------------------------------------------------------------------
@@ -67,9 +152,10 @@ meshserver::common::JobDetails Worker::getJob()
 
   zmq::pollitem_t item =  { this->Broker,  0, ZMQ_POLLIN, 0 };
 
-  canMesh.send(this->Broker);
   while(true)
     {
+    //send the request for a mesh
+    canMesh.send(this->Broker);
     zmq::poll(&item,1,-1);
     if(item.revents & ZMQ_POLLIN)
       {
@@ -80,15 +166,12 @@ meshserver::common::JobDetails Worker::getJob()
       std::string msg = response.dataAs<std::string>();
 
       //see if we are given a job, or a request to poll again
-      if(msg == meshserver::INVALID_MSG)
+      if(msg != meshserver::INVALID_MSG)
         {
-        //send the request for a mesh again
-        canMesh.send(this->Broker);
-        }
-      else
-        {
-        return meshserver::to_JobDetails(msg);
-        }
+        const meshserver::common::JobDetails temp = meshserver::to_JobDetails(msg);
+        this->CurrentJobIds.insert(temp.JobId);
+        return temp;
+        }      
       }
     }
 }
@@ -113,6 +196,9 @@ void Worker::returnMeshResults(const meshserver::common::JobResult& result)
                                     meshserver::RETRIEVE_MESH,
                                     msg.data(),msg.size());
   message.send(this->Broker);
+
+  //remove the id from our current list of jobs we are doing
+  this->CurrentJobIds.erase(result.JobId);
 }
 
 
