96847a612802385d9e2407635245bb9729286a5b
Robert Maynard <robert.maynard@kitware.com>
2012-04-05 15:23:00 -0400

    upgrade the worker test to show we can handle assign a worker multiple jobs.
    We support a worker processing multiple jobs at a time, and a worker
    requesting more jobs once finished.

--- Testing/workerMain.cxx
+++ Testing/workerMain.cxx
@@ -7,29 +7,58 @@
 =========================================================================*/
 
 #include <meshserver/worker/Worker.h>
+#include <vector>
 
-int main ()
+int main (int argc, char* argv[])
 {
-  meshserver::worker::Worker w(meshserver::MESH2D);
+  int timesToAskForJobs = 1;
+  int jobsToAskForEachTime =1;
 
-  for(int i=0; i < 4; ++i)
-  {
-  meshserver::common::JobDetails jd = w.getJob();
-
-  meshserver::common::JobStatus status(jd.JobId,meshserver::IN_PROGRESS);
-  for(int i=1; i <= 100; ++i)
+  //first argument is the number of times to as the broker for jobs
+  //the second argument is the number of jobs each time to ask for from the broker
+  //both default to 1
+  if (argc == 3)
     {
-    status.Progress = i;
-    if(i%20==0)
-      sleep(1);
-    w.updateStatus(status);
+    jobsToAskForEachTime = atoi(argv[2]);
+    }
+  if (argc >= 2)
+    {
+    timesToAskForJobs = atoi(argv[1]);
     }
 
-  status = meshserver::common::JobStatus(jd.JobId,meshserver::FINISHED);
-  w.updateStatus(status);
+  meshserver::worker::Worker w(meshserver::MESH2D);
 
-  meshserver::common::JobResult results(jd.JobId,"FAKE RESULTS");
-  w.returnMeshResults(results);
+  for(int i=0; i < timesToAskForJobs; ++i)
+  {
+    std::vector<meshserver::common::JobDetails> jobDetails;
+    for(int j=0; j < jobsToAskForEachTime; ++j)
+      {
+      jobDetails.push_back(w.getJob());
+      }
+
+    for(int progress=1; progress <= 100; ++progress)
+      {
+      if(progress%20==0)
+        sleep(1);
+      for(int j=0; j < jobsToAskForEachTime; ++j)
+        {
+        meshserver::common::JobStatus status(jobDetails[j].JobId,meshserver::IN_PROGRESS);
+        status.Progress = progress;
+        w.updateStatus(status);
+        }
+      }
+
+    for(int j=0; j < jobsToAskForEachTime; ++j)
+      {
+      meshserver::common::JobStatus status(jobDetails[j].JobId,meshserver::FINISHED);
+      w.updateStatus(status);
+      }
+
+    for(int j=0; j < jobsToAskForEachTime; ++j)
+      {
+      meshserver::common::JobResult results(jobDetails[j].JobId,"FAKE RESULTS");
+      w.returnMeshResults(results);
+      }
   }
 
   return 1;
