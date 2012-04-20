5629078260d18f8f8d789b5c49550aca6e6e1f7e
Robert Maynard <robert.maynard@kitware.com>
2012-04-20 12:04:22 -0400

    Only build the parts of boost we use, not the entire thing.

--- CMake/boost.cmake
+++ CMake/boost.cmake
@@ -3,6 +3,7 @@ add_external_project(boost
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND <SOURCE_DIR>/bootstrap.sh
     --prefix=<INSTALL_DIR>
+    --with-libraries=date_time,exception,random,regex,system,thread
   BUILD_COMMAND pwd
-  INSTALL_COMMAND <SOURCE_DIR>/bjam install
+  INSTALL_COMMAND <SOURCE_DIR>/b2 install
 )
