dfc8f0346f22f7e33538d4678b5e552f6ea9a00c
Robert Maynard <robert.maynard@kitware.com>
2012-04-23 13:06:31 -0400

    Adding in windows version of the boost project.

--- CMake/win32/boost.cmake
+++ CMake/win32/boost.cmake
@@ -1,9 +1,9 @@
 #Boost bjam version
 add_external_project(boost
   BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND "<SOURCE_DIR>/bootstrap.sh
+  CONFIGURE_COMMAND "<SOURCE_DIR>/bootstrap.bat
     --prefix=<INSTALL_DIR>
     --with-libraries=filesystem,system,thread"
-  BUILD_COMMAND pwd
-  INSTALL_COMMAND <SOURCE_DIR>/b2 install
+  BUILD_COMMAND "<SOURCE_DIR>/b2 install"
+  INSTALL_COMMAND "pwd"
 )
