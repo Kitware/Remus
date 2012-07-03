9dd177346c680d35a2d389f207673b7c6b0371ae
Robert Maynard <robert.maynard@kitware.com>
2012-07-03 14:27:48 -0400

    Update the JobStatus object to better handle progress.
    This is done by adding a new class called JobProgress.
    
    Change-Id: Iebf7aacd661f222e1e11cdfc8699b258ccadf1f7

--- remus/JobStatus.h
+++ remus/JobStatus.h
@@ -22,42 +22,132 @@
 
 #include <remus/common/remusGlobals.h>
 
-namespace
-{
-inline int valid_progress_value(int v)
-{
-  v = std::min<int>(v,100);
-  v = std::max<int>(v,1);
-  return v;
-}
-}
-
 namespace remus {
+
+//Job progress is a helper class to easily state what the progress of a currently
+//running job is. Progress can be numeric, textual or both.
+struct JobProgress
+{
+  JobProgress():
+    Value(-1),
+    Message()
+    {}
+
+  explicit JobProgress(remus::STATUS_TYPE status):
+    Value(-1),
+    Message()
+    {
+    if(status==remus::IN_PROGRESS)
+      {
+      //if this is a valid in_progress job progress struct, than make
+      //value 0
+      this->Value = 0;
+      }
+    }
+
+  explicit JobProgress(int value):
+    Value(valid_progress_value(value)),
+    Message()
+    {}
+
+  explicit JobProgress(const std::string& message):
+    Value(0),
+    Message(message)
+    {}
+
+  JobProgress(int value, const std::string& message):
+    Value(valid_progress_value(value)),
+    Message(message)
+    {}
+
+  //overload on the progress object to make it easier to detect when
+  //progress has been changed.
+  bool operator ==(const JobProgress& b) const
+  {
+    return this->Value == b.Value && this->Message == b.Message;
+  }
+
+  //overload on the progress object to make it easier to detect when
+  //progress has been changed.
+  bool operator !=(const JobProgress& b) const
+  {
+    return !(this->operator ==(b));
+  }
+
+  int value() const { return Value; }
+  const std::string message() const { return Message; }
+
+  void setValue(int value) { Value = JobProgress::valid_progress_value(value); }
+  void setMessage(const std::string& msg) { Message = msg; }
+
+
+  //make sure that we can't set progress to be outside the valid range.
+  static inline int valid_progress_value(int v)
+  {
+    v = std::min<int>(v,100);
+    v = std::max<int>(v,1);
+    return v;
+  }
+
+  //make it easy to print out the progress, by overloading the << operator
+  //when using cout, cerr, etc
+  friend std::ostream& operator<< (std::ostream& out, const JobProgress& j)
+  {
+    if(j.Value < 0)
+      {
+      out << "Progress: Invalid";
+      }
+    else if(j.Value > 0 && j.Message.size() > 0)
+      {
+      out << "Progress[" << j.Value << "%]: " << j.Message;
+      }
+    else if(j.Value > 0)
+      {
+      out << "Progress: " << j.Value << "%";
+      }
+    else
+      {
+      out << "Progress: " << j.Message;
+      }
+    return out;
+  }
+
+private:
+  int Value;
+  std::string Message;
+};
+
 struct JobStatus
 {
   boost::uuids::uuid JobId;
   remus::STATUS_TYPE Status;
-  int Progress;
+  remus::JobProgress Progress;
 
+  //Construct an invalid job status message
   JobStatus():
     JobId(),
     Status(remus::INVALID_STATUS),
-    Progress(-1)
+    Progress(remus::INVALID_STATUS)
     {}
 
+  //Construct a job status that has no form of progress value or message
   JobStatus(const boost::uuids::uuid& id, remus::STATUS_TYPE stat):
     JobId(id),
     Status(stat),
-    Progress(0)
-    {}
+    Progress(stat)
+    {
+    }
 
   //will make sure that the progress value is between 1 and 100 inclusive
   //on both ends. Progress value of zero is used when the status type is not progress
-  JobStatus(const boost::uuids::uuid& id, remus::STATUS_TYPE stat, int progress):
+  JobStatus(const boost::uuids::uuid& id, const remus::JobProgress& progress):
     JobId(id),
-    Status(stat),
-    Progress( valid_progress_value(progress) )
-    {}
+    Status(remus::IN_PROGRESS),
+    Progress(progress)
+    {
+
+    }
+
 };
 
 //------------------------------------------------------------------------------
@@ -67,7 +157,14 @@ inline std::string to_string(const remus::JobStatus& status)
   std::stringstream buffer;
   buffer << status.JobId << std::endl;
   buffer << status.Status << std::endl;
-  buffer << status.Progress << std::endl;
+
+  //only send progress info, if we are actually a status message that
+  //cares about that information
+  if(status.Status == remus::IN_PROGRESS)
+    {
+    buffer << status.Progress.value() << std::endl;
+    buffer << status.Progress.message() << std::endl;
+    }
   return buffer.str();
 }
 
@@ -79,15 +176,25 @@ inline remus::JobStatus to_JobStatus(const std::string& status)
 
   boost::uuids::uuid id;
   int t;
-  int p;
-
   buffer >> id;
   buffer >> t;
-  buffer >> p;
 
   const remus::STATUS_TYPE type = static_cast<remus::STATUS_TYPE>(t);
+  if(type!=remus::IN_PROGRESS)
+    {
+    return remus::JobStatus(id,type);
+    }
+  else
+    {
+    //if we are progress status message we have two more pieces of info to decode
+    int progressValue;
+    std::string progressMessage;
 
-  return remus::JobStatus(id,type,p);
+    buffer >> progressValue;
+    buffer >> progressMessage;
+    JobProgress pr(progressValue,progressMessage);
+    return remus::JobStatus(id,pr);
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -96,5 +203,6 @@ inline remus::JobStatus to_JobStatus(const char* data, int size)
   //convert a job status from a string, used as a hack to serialize
   return to_JobStatus( std::string(data,size) );
 }
+
 }
 #endif
