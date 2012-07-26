2757b443c93dfe0f09363881715006beefc5f536
Robert Maynard <robert.maynard@kitware.com>
2012-07-26 15:23:28 -0400

    The boost install prefix command doesn't support escaped spaces.
    The intall step on windows the exception to getting directories with
    spaces to be supported. Instead you just need to quote the directory.

--- Projects/win32/boost.cmake
+++ Projects/win32/boost.cmake
@@ -13,24 +13,27 @@ set(boost_with_args
 
 #since we don't specify a prefix for the superbuild,
 #we can determine where the buld directory will be. This
-#is needed as we need to wrap the build directory in quotes to 
+#is needed as we need to wrap the build directory in quotes to
 #and escape spaces in the path for boost to properly build.
 string(REPLACE " " "\\ " boost_build_dir ${CMAKE_CURRENT_BINARY_DIR}/boost/src/boost)
-string(REPLACE " " "\\ " boost_install_dir ${install_location})
+
+#we don't convert the install path on windows, as for the install step it can't
+#handle escaping the spaces. I have no clue why boost is
+#such a mess on support for directories with spaces in the name
 
 
 set(boost_cmds
   CONFIGURE_COMMAND bootstrap.bat
   BUILD_COMMAND b2 --build-dir=${boost_build_dir} address-model=${am} ${boost_with_args}
   INSTALL_COMMAND b2 address-model=${am} ${boost_with_args}
-    --prefix=${boost_install_dir} install
+    --prefix=${install_location} install
   )
 
 add_external_project(boost
   ${boost_cmds}
   BUILD_IN_SOURCE 1
   )
-  
+
   #install header files
   ExternalProject_Add_Step(boost fixBoostInstallStage1
     COMMAND ${CMAKE_COMMAND} -E copy_directory <INSTALL_DIR>/include/boost-1_49/boost <INSTALL_DIR>/include/boost
@@ -39,7 +42,7 @@ add_external_project(boost
   ExternalProject_Add_Step(boost fixBoostInstallStage2
     COMMAND ${CMAKE_COMMAND} -E remove_directory <INSTALL_DIR>/include/boost-1_49
     DEPENDEES fixBoostInstallStage1
-    )  
+    )
 
 ExternalProject_Get_Property(boost install_dir)
 add_project_property(boost BOOST_INCLUDEDIR "${install_dir}/include/boost")
