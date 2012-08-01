1d79faa9c8bf5159d8a6a0bd4d9d40743a313c7b
Robert Maynard <robert.maynard@kitware.com>
2012-08-01 09:24:22 -0400

    Corrected build issues with zeroMQ in directories with spaces in the name.
    The zeroMQ configure script will refuse to run if executed with an absolute path
    that is quoted which is what happens when their is a space in the path.

--- Projects/zeroMQ.cmake
+++ Projects/zeroMQ.cmake
@@ -1,7 +1,13 @@
 
+#we have to build in the source directory on Linux and OSX
+#If the path to the src directory has a space, CMake will quote
+#the path, which than the zeroMQ configure script will reject
+#as it doesn't allow quoted paths to the configure script.
+
 add_external_project(zeroMQ
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
     --prefix=<INSTALL_DIR>
+  BUILD_IN_SOURCE 1
 )
 
 ExternalProject_Get_Property(zeroMQ install_dir)
