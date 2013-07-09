7c83f04ae5fc92715ab308550fa7498f07202f8f
Robert Maynard <robert.maynard@kitware.com>
2013-07-09 12:31:52 -0400

    Add Worker that takes jobs forever and a server than has no factory.

---
+++ Examples/InfiniteWorker/workerMain.cxx
@@ -0,0 +1,57 @@
+/*=========================================================================
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include <remus/worker/Worker.h>
+#include <vector>
+#include <string>
+#include <iostream>
+
+#ifndef _WIN32
+# include <unistd.h>
+#else
+#include <windows.h>
+#endif
+
+int main (int argc, char* argv[])
+{
+  remus::worker::ServerConnection connection;
+  if(argc>=2)
+    {
+    //let the server connection handle parsing the command arguments
+    connection = remus::worker::ServerConnection(std::string(argv[1]));
+    }
+
+  remus::common::MeshIOType requirements(remus::RAW_EDGES,remus::MESH2D);
+  remus::Worker w(requirements,connection);
+
+  while(true)
+  {
+    remus::Job jd = w.getJob();
+
+    remus::JobStatus status(jd.id(),remus::IN_PROGRESS);
+    for(int progress=1; progress <= 100; ++progress)
+      {
+      if(progress%20==0)
+        {
+#ifdef _WIN32
+        Sleep(1000);
+#else
+        sleep(1);
+#endif
+        status.Progress.setValue(progress);
+        status.Progress.setMessage("Example Message With Random Content");
+        w.updateStatus(status);
+        }
+      }
+
+    remus::JobResult results(jd.id(),"FAKE RESULTS");
+    w.returnMeshResults(results);
+  }
+
+  return 1;
+}
---
+++ Examples/NoWorkerServer/serverMain.cxx
@@ -0,0 +1,24 @@
+/*=========================================================================
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include <remus/server/Server.h>
+#include <remus/server/WorkerFactory.h>
+int main ()
+{
+  //create a custom worker factory that creates children processes
+  //we cap it at having only 3 children at any time
+  remus::server::WorkerFactory factory;
+  factory.setMaxWorkerCount(0);
+
+  //create a default server with the factory
+  remus::server::Server b(factory);
+
+  //start accepting connections for clients and workers
+  bool valid = b.startBrokering();
+  return valid ? 0 : 1;
+}
