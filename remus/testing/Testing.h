da1685724c6f3195bf7ed81cd84342315b37558a
Robert Maynard <robert.maynard@kitware.com>
2013-10-15 15:23:18 -0400

    Add testing helper that updates a bool instead of exiting.
    This allows us to test for multiple failures before deciding if we want to exit
    
    Change-Id: Iacac85b3306846720940602ab6a82d6d28c81207

--- remus/testing/Testing.h
+++ remus/testing/Testing.h
@@ -25,10 +25,23 @@
 /// Asserts a condition for a test to pass. A passing condition is when \a
 /// condition resolves to true. If \a condition is false, then the test is
 /// aborted and failure is returned.
-
 #define REMUS_ASSERT(condition) \
-  if(!condition) { std::cerr << "Error at file: "  << __FILE__ << std::cout; \
-    std::cerr<<  "On line: " <<  __LINE__ << std::endl; exit(1); }
+  if(!condition) \
+    { std::cerr << "Error at file: "  << __FILE__ << std::cout; \
+    std::cerr <<  "On line: " <<  __LINE__ << std::endl; exit(1); }
+
+
+///  A passing condition is when \a condition resolves to true.
+///  If \a condition is false, then the valid_flag is set to false
+///  and we output to cerr which line we failed on.
+///  If the \a condition is true the valid flag is not modified
+#define REMUS_VALID(condition, valid_flag) \
+  valid_flag &= condition; \
+  if(!condition) \
+    { std::cerr << "Error at file: "  << __FILE__ << std::cout; \
+    std::cerr <<  "On line: " <<  __LINE__ << std::endl; \
+    }
+
 
 
 #endif //__remus_testing_Testing
