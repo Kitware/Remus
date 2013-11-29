f3d092cf5fd382a077227711d2e09cbda0309e6f
Robert Maynard <robert.maynard@kitware.com>
2013-11-29 11:14:25 -0500

    Update the basic client to report outstanding jobs and query throughput.
    Change-Id: I78bfd4f75c5048925e7a5f1c322e517dfa1532f7

--- examples/BasicClient/clientMain.cxx
+++ examples/BasicClient/clientMain.cxx
@@ -10,14 +10,68 @@
 
 #include <vector>
 #include <iostream>
+
+#ifndef _WIN32
+  #include <limits.h>
+  #include <sys/time.h>
+  #include <unistd.h>
+#endif
+
+#include <boost/cstdint.hpp>
+
+class Timer
+{
+public:
+  Timer() { this->Reset(); }
+
+  void Reset() { this->StartTime = this->GetCurrentTime(); }
+
+  float GetElapsedTime()
+    {
+    TimeStamp currentTime = this->GetCurrentTime();
+    float elapsedTime;
+    elapsedTime = currentTime.Seconds - this->StartTime.Seconds;
+    elapsedTime += ((currentTime.Microseconds - this->StartTime.Microseconds)
+                    /float(1000000));
+    return elapsedTime;
+    }
+
+  struct TimeStamp {
+    boost::int64_t Seconds;
+    boost::int64_t Microseconds;
+  };
+  TimeStamp StartTime;
+
+  TimeStamp GetCurrentTime()
+  {
+    TimeStamp retval;
+#ifdef _WIN32
+    timeb currentTime;
+    ::ftime(&currentTime);
+    retval.Seconds = currentTime.time;
+    retval.Microseconds = 1000*currentTime.millitm;
+#else
+    timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+    retval.Seconds = currentTime.tv_sec;
+    retval.Microseconds = currentTime.tv_usec;
+#endif
+    return retval;
+  }
+};
+
 int main ()
 {
+  //keep track of the number of queries to do a rough test
+  int queries = 0;
+
   //create a default server connection
   remus::client::ServerConnection conn;
 
   //create a client object that will connect to the default remus server
   remus::Client c(conn);
 
+  Timer time;
   //we create a basic job request for a mesh2d job, with the data contents of "TEST"
   remus::client::JobRequest request(remus::RAW_EDGES, remus::MESH2D,
                                     "TEST THE APPLICATION BY PASSING SPACES");
@@ -26,7 +80,7 @@ int main ()
     //if we can mesh 2D mesh jobs, we are going to submit 8 jobs to the server
     //for meshing
     std::vector<remus::client::Job> jobs;
-    for(int i=0; i < 8; ++i)
+    for(int i=0; i < 18; ++i, ++queries)
       {
       remus::client::Job job = c.submitJob(request);
       jobs.push_back(job);
@@ -34,7 +88,7 @@ int main ()
 
     //get the initial status of all the jobs that we have
     std::vector<remus::client::JobStatus> js;
-    for(int i=0; i < jobs.size(); ++i)
+    for(int i=0; i < jobs.size(); ++i, ++queries)
       {
       js.push_back(c.jobStatus(jobs.at(i)));
       }
@@ -43,7 +97,7 @@ int main ()
     //each time the status of the job changes
     while(jobs.size() > 0)
       {
-      for(int i=0; i < jobs.size(); ++i)
+      for(int i=0; i < jobs.size(); ++i, ++queries)
         {
         //update the status with the latest status and compare it too
         //the previously held status
@@ -72,9 +126,15 @@ int main ()
           {
           jobs.erase(jobs.begin()+i);
           js.erase(js.begin()+i);
+
+          std::cout << "outstanding jobs are: " << std::endl;
+          for(int i=0; i < jobs.size(); ++i)
+            { std::cout << "  " << jobs.at(i).id() << std::endl; }
           }
         }
       }
+    std::cout << "We issued " << queries <<  " queries to the server " << std::endl;
+    std::cout << "Number of queries per second is " << queries / time.GetElapsedTime() << std::endl;
     }
   else
     {
