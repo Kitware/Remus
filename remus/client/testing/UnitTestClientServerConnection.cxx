e1a50abb38d6633c0128f91ee032b0f4cfad7b37
Robert Maynard <robert.maynard@kitware.com>
2014-01-28 09:47:47 -0500

    Fix some warnings int the ServerConnection test.
    Change-Id: I60b0d4858a1d97aae06a125fdfd2e1fdfef68c94

--- remus/client/testing/UnitTestClientServerConnection.cxx
+++ remus/client/testing/UnitTestClientServerConnection.cxx
@@ -39,7 +39,7 @@ int UnitTestClientServerConnection(int, char *[])
 {
 
   remus::client::ServerConnection sc;
-  REMUS_ASSERT(sc.endpoint().size() > 0);
+  REMUS_ASSERT( (sc.endpoint().size() > 0) );
   const std::string default_endpoint = sc.endpoint();
 
   zmq::socketInfo<zmq::proto::tcp> default_socket("127.0.0.1",
