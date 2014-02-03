5d302c53f7e397429752849e3b626a7243c94f9e
Robert Maynard <robert.maynard@kitware.com>
2014-02-03 09:59:29 -0500

    Add in the new job submission class.
    Change-Id: I19b70edd41697d781a7414eda2d5145b0c5a9368

---
+++ remus/client/JobSubmission.cxx
@@ -0,0 +1,123 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//=============================================================================
+
+#include <remus/client/JobSubmission.h>
+#include <remus/common/remusGlobals.h>
+
+#include <algorithm>
+
+namespace remus{
+namespace client{
+
+//------------------------------------------------------------------------------
+JobSubmission::JobSubmission(  ):
+  MeshType( ),
+  Requirements( ),
+  Content()
+{
+}
+
+//------------------------------------------------------------------------------
+JobSubmission::JobSubmission( const remus::client::JobMeshRequirements& reqs ):
+  MeshType(reqs.jobMeshTypes()),
+  Requirements(reqs),
+  Content()
+{
+}
+
+//------------------------------------------------------------------------------
+JobSubmission::JobSubmission( const remus::client::JobMeshRequirements& reqs,
+                              const remus::client::JobContent& content ):
+  MeshType(reqs.jobMeshTypes()),
+  Requirements(reqs),
+  Content( )
+{
+  this->Content["default"]=content;
+}
+
+//------------------------------------------------------------------------------
+JobSubmission::JobSubmission( const remus::client::JobMeshRequirements& reqs,
+            const std::map<std::string,remus::client::JobContent>& content ):
+  MeshType(reqs.jobMeshTypes()),
+  Requirements(reqs),
+  Content(content)
+{
+}
+
+
+//------------------------------------------------------------------------------
+bool JobSubmission::operator<(const JobSubmission& other) const
+{
+
+  if ( !(this->type() == other.type()))
+  { return (this->type() < other.type()); }
+
+  if (!(this->retrieveRequirements() == other.retrieveRequirements()))
+  { return (this->retrieveRequirements() < other.retrieveRequirements()); }
+
+  if (this->Content.size() != other.Content.size())
+  {  return this->Content < other.Content; }
+
+ return false; //both objects are equal
+}
+
+//------------------------------------------------------------------------------
+bool JobSubmission::operator==(const JobSubmission& other) const
+ {
+  //we need to think about this more, a job submission isn't really unique
+  //if we don't look at the submitted job content. You can submit the same
+  return ((this->type() == other.type()) &&
+          (this->retrieveRequirements() == other.retrieveRequirements()) &&
+          (this->Content.size() == other.Content.size()) &&
+          (this->Content == other.Content)
+          );
+ }
+
+//------------------------------------------------------------------------------
+void JobSubmission::serialize(std::ostream& buffer) const
+{
+  buffer << this->MeshType << std::endl;
+  buffer << this->Requirements << std::endl;
+  buffer << this->Content.size() << std::endl;
+  for(JobSubmission::const_iterator i = this->begin();
+      i != this->end();
+      ++i)
+    {
+    buffer << i->first.size() << std::endl;;
+    remus::internal::writeString(buffer,i->first);
+    buffer << i->second << std::endl;
+    }
+}
+
+//------------------------------------------------------------------------------
+JobSubmission::JobSubmission(std::istream& buffer)
+{
+  std::size_t contentSize=0;
+  buffer >> this->MeshType;
+  buffer >> this->Requirements;
+  buffer >> contentSize;
+  for(std::size_t i = 0; i < contentSize; ++i)
+    {
+    std::size_t keySize=0;
+    buffer >> keySize;
+
+    std::string key = remus::internal::extractString(buffer,keySize);
+
+    JobContent value;
+    buffer >> value;
+    this->Content[key]=value;
+    }
+}
+
+
+}
+}
