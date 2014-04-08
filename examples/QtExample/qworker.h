45029d762fe37976fa2dc3dfbbd942e141ec264e
Robert Maynard <robert.maynard@kitware.com>
2014-04-08 16:07:22 -0400

    Add in example of using all three parts of the remus interface in a qt app.
    Change-Id: I88ec36d13421bce83b20f0ba3176903e4dbe27f8

---
+++ examples/QtExample/qworker.h
@@ -0,0 +1,48 @@
+/*=========================================================================
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef qworker_h
+#define qworker_h
+
+#include <QObject>
+#include <QThread>
+#include <QScopedPointer>
+
+namespace remus { namespace worker {  class Worker; }}
+
+
+class RWorker : public QObject
+{
+  Q_OBJECT
+  QThread workerThread;
+public:
+  RWorker();
+  ~RWorker();
+
+public slots:
+  void stopProcessing();
+  void startProcessing();
+private:
+  bool DoWork;
+  QScopedPointer<remus::worker::Worker> Worker;
+};
+
+class qworker : public QObject
+{
+  Q_OBJECT
+  QThread workerThread;
+
+public:
+  qworker();
+  ~qworker();
+
+signals:
+  void startProcessing();
+};
+
+#endif
