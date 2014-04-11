7056466776d5dad991642d246cf497d068a7af52
Robert Maynard <robert.maynard@kitware.com>
2014-04-11 13:45:23 -0400

    Introducing the SocketMonitor the advanced way to determine when workers die.
    Change-Id: Ia765cb431a32d5572e43eb5f55dc1822c35a6d0e

---
+++ remus/server/detail/SocketMonitor.cxx
@@ -0,0 +1,186 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//=============================================================================
+
+#include <remus/server/detail/SocketMonitor.h>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+namespace remus{
+namespace server{
+namespace detail{
+
+//------------------------------------------------------------------------------
+class SocketMonitor::WorkerTracker
+{
+  typedef boost::posix_time::ptime ptime;
+
+  struct BeatInfo { boost::int64_t Duration; ptime LastOccurrence; };
+public:
+  remus::common::PollingMonitor PollMonitor;
+
+  std::map< zmq::SocketIdentity, BeatInfo > HeartBeats;
+
+  typedef std::pair< zmq::SocketIdentity, BeatInfo > InsertType;
+  typedef std::map< zmq::SocketIdentity, BeatInfo >::iterator IteratorType;
+
+  WorkerTracker( remus::common::PollingMonitor p):
+    PollMonitor(p)
+  {}
+
+  //----------------------------------------------------------------------------
+  remus::common::PollingMonitor monitor() const { return PollMonitor; }
+
+
+  //----------------------------------------------------------------------------
+  bool exists( const zmq::SocketIdentity& socket ) const
+    { return this->HeartBeats.count(socket) > 0; }
+
+  //----------------------------------------------------------------------------
+  void refresh(const zmq::SocketIdentity& socket)
+  {
+    //insert a new item if it doesn't exist, otherwise get the beatInfo already
+    //in the map
+    InsertType key_value(socket,BeatInfo());
+    IteratorType iter = (this->HeartBeats.insert(key_value)).first;
+    BeatInfo& beat = iter->second;
+
+    beat.LastOccurrence = boost::posix_time::microsec_clock::local_time();
+
+    //update our duration to be the next timeout for the poller, since if
+    //we are refreshing a socket we are getting regular communication from it
+    beat.Duration  = (PollMonitor.hasAbnormalEvent() ? PollMonitor.maxTimeOut() : PollMonitor.current());
+  }
+
+  //----------------------------------------------------------------------------
+  void heartbeat( const zmq::SocketIdentity& socket, int64_t dur )
+  {
+    //insert a new item if it doesn't exist, otherwise get the beatInfo already
+    //in the map
+    InsertType key_value(socket,BeatInfo());
+    IteratorType iter = (this->HeartBeats.insert(key_value)).first;
+    BeatInfo& beat = iter->second;
+
+    beat.LastOccurrence = boost::posix_time::microsec_clock::local_time();
+
+    //Now we choose the greatest value between the poller and the sent in duration
+    //from the socket
+    const boost::int64_t polldur = (PollMonitor.hasAbnormalEvent() ? PollMonitor.maxTimeOut() : PollMonitor.current());
+    beat.Duration = std::max(dur,polldur);
+  }
+
+  //----------------------------------------------------------------------------
+  void markAsDead( const zmq::SocketIdentity& socket )
+  {
+    this->HeartBeats.erase(socket);
+  }
+
+  //----------------------------------------------------------------------------
+  bool isMostlyDead( const zmq::SocketIdentity& socket ) const
+  {
+    if(this->exists(socket))
+      {
+      //polling has been abnormal give it a pass
+      if(PollMonitor.hasAbnormalEvent()) { return false; }
+
+      const BeatInfo& beat = this->HeartBeats.find(socket)->second;
+      const ptime current = boost::posix_time::microsec_clock::local_time();
+      const ptime expectedHB = beat.LastOccurrence +
+                               boost::posix_time::seconds(beat.Duration*2);
+      return current > expectedHB;
+      }
+
+    //the socket isn't contained here, this socket is dead dead
+    return true;
+  }
+};
+
+//------------------------------------------------------------------------------
+SocketMonitor::SocketMonitor():
+  Tracker(new WorkerTracker( (remus::common::PollingMonitor()) ))
+{
+
+}
+
+//------------------------------------------------------------------------------
+SocketMonitor::SocketMonitor( remus::common::PollingMonitor pMonitor ):
+  Tracker(new WorkerTracker( pMonitor ))
+{
+
+}
+
+//------------------------------------------------------------------------------
+SocketMonitor::SocketMonitor( const SocketMonitor& other ):
+  Tracker(other.Tracker)
+{
+
+}
+
+//------------------------------------------------------------------------------
+SocketMonitor& SocketMonitor::operator= (SocketMonitor other)
+{
+  this->Tracker.swap(other.Tracker);
+  return *this;
+}
+
+
+//------------------------------------------------------------------------------
+SocketMonitor::~SocketMonitor()
+{
+
+}
+
+//------------------------------------------------------------------------------
+remus::common::PollingMonitor SocketMonitor::pollingMonitor() const
+{
+  return this->Tracker->monitor();
+}
+
+//------------------------------------------------------------------------------
+void SocketMonitor::refresh( const zmq::SocketIdentity& socket )
+{
+  this->Tracker->refresh(socket);
+}
+
+//------------------------------------------------------------------------------
+void SocketMonitor::heartbeat( const zmq::SocketIdentity& socket,
+                               const remus::proto::Message& msg )
+{
+  //convert the string back into an int64_t which represents seconds to
+  //the next heartbeat expected from this socket
+  std::string messagePayload(msg.data(),msg.dataSize());
+  boost::int64_t dur_in_secs = boost::lexical_cast<boost::int64_t>(
+                                                              messagePayload);
+  this->Tracker->heartbeat(socket, dur_in_secs);
+}
+
+//------------------------------------------------------------------------------
+void SocketMonitor::markAsDead( const zmq::SocketIdentity& socket )
+{
+  //we need to explicitly mark a socket as dead
+  return this->Tracker->markAsDead(socket);
+}
+
+//------------------------------------------------------------------------------
+bool SocketMonitor::isDead( const zmq::SocketIdentity& socket ) const
+{
+  return !this->Tracker->exists(socket);
+}
+
+//------------------------------------------------------------------------------
+bool SocketMonitor::isUnresponsive( const zmq::SocketIdentity& socket ) const
+{
+  return this->Tracker->isMostlyDead(socket);
+}
+
+}
+}
+}
---
+++ remus/server/detail/SocketMonitor.h
@@ -0,0 +1,73 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//=============================================================================
+
+#ifndef remus_server_detail_SocketMonitor_h
+#define remus_server_detail_SocketMonitor_h
+
+#include <boost/shared_ptr.hpp>
+
+#include <remus/proto/Message.h>
+#include <remus/proto/zmqSocketIdentity.h>
+
+#include <remus/common/PollingMonitor.h>
+
+namespace remus{
+namespace server{
+namespace detail{
+
+// Provides monitoring that adjusts to the polling frequency of socket ids
+class SocketMonitor
+{
+public:
+  SocketMonitor( );
+  SocketMonitor( remus::common::PollingMonitor pollingMonitor );
+  SocketMonitor( const SocketMonitor& other );
+
+  SocketMonitor& operator= (SocketMonitor other);
+
+  ~SocketMonitor();
+
+  remus::common::PollingMonitor pollingMonitor() const;
+
+  //refresh a socket stating it is alive. Uses the pollingMontior
+  //to determine the expect time of the next heartbeat from the socket.
+  void refresh( const zmq::SocketIdentity& socket);
+
+  //update a sockets heartbeat duration, marks the socket as alive.
+  //Compares the heart beat interval in the message and the pollingMontior
+  //to determine the expect time of the next heartbeat from the socket
+  void heartbeat( const zmq::SocketIdentity& socket,
+                  const remus::proto::Message& msg );
+
+  //we have been told by the socket it is shutting down, so we mark
+  //the socket as fully dead.
+  void markAsDead( const zmq::SocketIdentity& socket );
+
+  //returns true if a socket is fully dead, and not mostly-dead
+  bool isDead( const zmq::SocketIdentity& socket ) const;
+
+  //returns true if a socket is not fully dead, but mostly-dead
+  //this occurs when a socket has missed a heartbeat, but we some contextual
+  //info from the polling monitor that some abnormal behavior has happened,
+  //and we should expect sockets to come back.
+  bool isUnresponsive( const zmq::SocketIdentity& socket ) const;
+
+private:
+  class WorkerTracker;
+  boost::shared_ptr<WorkerTracker> Tracker;
+};
+
+}
+}
+}
+
+#endif
