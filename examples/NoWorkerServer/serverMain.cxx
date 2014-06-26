92b65bc045f3defa06958183cf125d6a12d54fd2
Robert Maynard <robert.maynard@kitware.com>
2014-06-26 09:08:54 -0400

    Fixes #14. We now have documentation on creating a custom WorkerFactory.
    Change-Id: Iaf4b0394c8cb02856116b589600d01d68736761e

--- examples/NoWorkerServer/serverMain.cxx
+++ examples/NoWorkerServer/serverMain.cxx
@@ -8,12 +8,53 @@
 
 #include <remus/server/Server.h>
 #include <remus/server/WorkerFactory.h>
+
+#include <remus/proto/JobRequirements.h>
+
+namespace factory
+{
+//we want a custom factory that acts like it can create workers when queried
+//but actually doesn't when asked too. This will allow the server to queue
+//jobs when no worker is connected, instead of rejecting all jobs that are
+//submitted
+class DoNothingFactory: public remus::server::WorkerFactory
+{
+public:
+  remus::proto::JobRequirementsSet workerRequirements(
+                                          remus::common::MeshIOType type) const
+  {
+    //return a fake set of requirements which makes it look like we can mesh
+    //the given input & output type.
+    remus::proto::JobRequirementsSet result;
+    remus::proto::JobRequirements reqs =
+        remus::proto::make_JobRequirements(type, std::string(), std::string());
+    result.insert(reqs);
+    return result;
+  }
+
+  bool haveSupport(const remus::proto::JobRequirements& reqs) const
+    {
+    //we want to return true here so that the server always queues
+    return true;
+    }
+
+  bool createWorker(const remus::proto::JobRequirements& type,
+                    WorkerFactory::FactoryDeletionBehavior lifespan)
+    {
+    //we want to return false here so that server never thinks we are creating
+    //a worker and assigns a job to a worker we didn't create
+    return false;
+    }
+};
+
+}
+
 int main ()
 {
-  //create a custom worker factory that creates children processes
-  //we cap it at having only 3 children at any time
-  remus::server::WorkerFactory factory;
-  factory.setMaxWorkerCount(0);
+  //create a custom worker factory that creates no workers but says it can mesh
+  //so that we queue jobs even when we have no workers connected.
+  boost::shared_ptr<factory::DoNothingFactory> factory(new factory::DoNothingFactory());
+  factory->setMaxWorkerCount(1);
 
   //create a default server with the factory
   remus::server::Server b(factory);
