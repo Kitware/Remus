92fa83e53b9ab15fd5c8674d0f88f3f5d3bfec9c
Brennon Brimhall <brennon.brimhall@kitware.com>
2014-07-23 16:26:25 -0400

    Initial attempt at getting CBadge support added to Remus.
    Change-Id: I24fe2dab2c1e72753ec17465b0ea3c3c8f4769ad

---
+++ travis.cmake
@@ -0,0 +1,63 @@
+set(CTEST_PROJECT_NAME "Remus")
+
+#What compiler are we using?
+if("$ENV{CXX}" STREQUAL "g++")
+	set(COMPILER "g++")
+	message("Using g++.")
+elseif("$ENV{CXX}" STREQUAL "clang++")
+	set(COMPILER "clang")
+	message("Using clang++")
+else()
+	set(COMPILER "unknown")
+endif()
+
+#Setting our build name and build configuration.
+string(SUBSTRING "$ENV{TRAVIS_COMMIT}" 0 7 SHA)
+set(CTEST_BUILD_NAME "${COMPILER}-$ENV{BUILD_CONFIG}-${SHA}")
+set(CTEST_BUILD_CONFIGURATION ${CMAKE_BUILD_TYPE})
+
+#Setting name of dashboard.
+set(CTEST_SITE "travis.travis-ci")
+
+#Setting directory paths.
+set (CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}")
+set (CTEST_BINARY_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/build")
+
+#Setting release/debug based on environment variable:
+set(CTEST_BUILD_CONFIGURATION $ENV{BUILD_CONFIG})
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+#Write initial cache:
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "
+CMAKE_BUILD_TYPE:STRING=$ENV{BUILD_CONFIG}
+Remus_NO_SYSTEM_BOOST:BOOl=OFF
+Remus_ENABLE_COVERAGE:BOOL=ON
+")
+
+#Use Travis track:
+ctest_start(Experimental TRACK "Travis" . )
+
+#Make sure we fetch correctly.
+set(CTEST_GIT_COMMAND "git")
+set(UPDATE_COMMAND ${CTEST_GIT_COMMAND})
+
+#Mirror Travis' checkout process:
+set(CTEST_GIT_UPDATE_OPTIONS "origin +refs/pull/$ENV{TRAVIS_PULL_REQUEST}/merge")
+set(CTEST_UPDATE_OPTIONS ${CTEST_GIT_UPDATE_OPTIONS})
+
+ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
+message("Updated.")
+ctest_configure(SOURCE "${CTEST_SOURCE_DIRECTORY}" BUILD "${CTEST_BINARY_DIRECTORY}" APPEND)
+message("Configured.")
+ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" APPEND)
+message("Built.")
+ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}" APPEND PARALLEL_LEVEL 6 SCHEDULE_RANDOM ON)
+message("Tested.")
+
+#For some reason, CTest doesn't automatically figure this out.  Manually set that we want gcov.
+set(CTEST_COVERAGE_COMMAND "gcov")
+
+ctest_coverage(BUILD "${CTEST_BINARY_DIRECTORY}" APPEND)
+message("Computed coverage.")
+ctest_submit()
+message("Submitted.")
