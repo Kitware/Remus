5203f7700197fd3cce2ecfe9431e93d6205e612b
Robert Maynard <robert.maynard@kitware.com>
2014-08-17 10:37:18 -0400

    Update the Registry test to verify we don't have overlapping mesh types.

--- remus/common/testing/UnitTestMeshRegistry.cxx
+++ remus/common/testing/UnitTestMeshRegistry.cxx
@@ -38,42 +38,74 @@ namespace
     REMUS_ASSERT( (t1->id() == t2->id()) );
     REMUS_ASSERT( (t1->name() == t2->name()) );
   }
-}
 
+  void verify_create()
+  {
+    using namespace remus::meshtypes;
+    VerifySame(Mesh1D::create(), to_meshType(1));
+    VerifySame(Mesh2D::create(), to_meshType(2));
+    VerifySame(Mesh3D::create(), to_meshType(3));
+    VerifySame(Mesh3DSurface::create(), to_meshType(4));
+    VerifySame(SceneFile::create(), to_meshType(5));
+    VerifySame(Model::create(), to_meshType(6));
+    VerifySame(Edges::create(), to_meshType(7));
+    VerifySame(PiecewiseLinearComplex::create(), to_meshType(8));
+  }
 
-int UnitTestMeshRegistry(int, char *[])
-{
-  RegisterTextMeshType();
-
+  void no_conflicting_ids_or_names()
+  {
   typedef boost::shared_ptr<remus::meshtypes::MeshTypeBase> MeshType;
-  MeshType base = remus::common::MeshRegistrar::instantiate(999);
-
-  VerifySame(base, TextMeshType::create() );
-  VerifySame(base, remus::meshtypes::to_meshType(999) );
-  VerifySame(base, remus::meshtypes::to_meshType("TextMeshType"));
-  VerifySame(TextMeshType::create(), remus::meshtypes::to_meshType(base->id()));
-
-  //verify a type that should have been added by the default static
-  //initialization code
-  base = remus::common::MeshRegistrar::instantiate(4);
-  VerifySame(base, remus::meshtypes::Mesh3DSurface::create() );
-  VerifySame(base, remus::meshtypes::to_meshType(4) );
-  VerifySame(base, remus::meshtypes::to_meshType("Mesh3DSurface"));
-  VerifySame(remus::meshtypes::Mesh3DSurface::create(),
-             remus::meshtypes::to_meshType(base->id()));
 
   std::set<MeshType> all_types = remus::common::MeshRegistrar::allRegisteredTypes();
   REMUS_ASSERT( (all_types.size() > 0) );
-  REMUS_ASSERT( (all_types.size() == 9) );
+  REMUS_ASSERT( (all_types.size() == 8) );
+
 
   for(std::set<MeshType>::const_iterator i = all_types.begin();
       i != all_types.end();
       ++i)
     {
-    //verify we don't have two types with the same name or id
+    //verify that the to_meshType works properly with any id and name
     VerifySame(*i,remus::meshtypes::to_meshType((*i)->name()));
     VerifySame(*i,remus::meshtypes::to_meshType((*i)->id()));
     }
+
+  std::set<boost::uint16_t> ids;
+  std::set<std::string> names;
+  for(std::set<MeshType>::const_iterator i = all_types.begin();
+      i != all_types.end();
+      ++i)
+    {
+    //verify we don't have two types with the same name or id
+    ids.insert((*i)->id());
+    names.insert((*i)->name());
+    }
+  REMUS_ASSERT( (all_types.size() == ids.size()) );
+  REMUS_ASSERT( (all_types.size() == names.size()) );
+  }
+
+  void can_add_custom_type()
+  {
+    RegisterTextMeshType();
+
+    typedef boost::shared_ptr<remus::meshtypes::MeshTypeBase> MeshType;
+    MeshType base = remus::common::MeshRegistrar::instantiate(999);
+
+    VerifySame(base, TextMeshType::create() );
+    VerifySame(base, remus::meshtypes::to_meshType(999) );
+    VerifySame(base, remus::meshtypes::to_meshType("TextMeshType"));
+    VerifySame(TextMeshType::create(), remus::meshtypes::to_meshType(base->id()));
+
+  }
+}
+
+
+
+int UnitTestMeshRegistry(int, char *[])
+{
+  verify_create();
+  no_conflicting_ids_or_names();
+  can_add_custom_type();
 
   return 0;
 }
