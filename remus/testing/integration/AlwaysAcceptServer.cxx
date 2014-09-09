30cad2bbcb011897c74c8afe84d1c311ddac59f5
Robert Maynard <robert.maynard@kitware.com>
2014-09-08 20:53:11 -0400

    Update all the integration tests to not share a context.
    Now that we don't need to share context's don't.

--- remus/testing/integration/AlwaysAcceptServer.cxx
+++ remus/testing/integration/AlwaysAcceptServer.cxx
@@ -135,8 +135,6 @@ boost::shared_ptr<remus::Client> make_Client( const remus::server::ServerPorts& 
 {
   remus::client::ServerConnection conn =
               remus::client::make_ServerConnection(ports.client().endpoint());
-  conn.context(ports.context());
-
   boost::shared_ptr<remus::Client> c(new remus::client::Client(conn));
   return c;
 }
@@ -149,7 +147,6 @@ boost::shared_ptr<remus::Worker> make_Worker( const remus::server::ServerPorts& 
 
   remus::worker::ServerConnection conn =
               remus::worker::make_ServerConnection(ports.worker().endpoint());
-  conn.context(ports.context());
 
   remus::common::MeshIOType io_type = remus::common::make_MeshIOType(Mesh2D(),Mesh3D());
   JobRequirements requirements = make_JobRequirements(io_type, "SimpleWorker", "");
@@ -348,8 +345,7 @@ int AlwaysAcceptServer(int argc, char* argv[])
   (void) argc;
   (void) argv;
 
-  //construct a simple worker and client, we need to share the same
-  //context between the server, client and worker
+  //construct a simple worker and client
   boost::shared_ptr<remus::Server> server = make_Server( remus::server::ServerPorts() );
   const remus::server::ServerPorts& ports = server->serverPortInfo();
 
