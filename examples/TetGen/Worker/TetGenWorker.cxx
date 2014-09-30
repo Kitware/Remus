5f4ef80ee8ad088f974e7a932e085fbdb6f6414f
Robert Maynard <robert.maynard@kitware.com>
2014-09-30 13:44:55 -0400

    Remove a duplicate main function in the tetgen worker example

--- examples/TetGen/Worker/TetGenWorker.cxx
+++ examples/TetGen/Worker/TetGenWorker.cxx
@@ -167,19 +167,3 @@ void TetGenWorker::meshJob()
   this->returnMeshResults(results);
   return;
 }
-
-
-int main (int argc, char* argv[])
-{
-  remus::worker::ServerConnection connection;
-  if(argc>=2)
-    {
-    //let the server connection handle parsing the command arguments
-    connection = remus::worker::make_ServerConnection(std::string(argv[1]));
-    }
-  //the triangle worker is hard-coded to only handle 3D meshes output, and
-  //input of PIECWISE_LINEAR_COMPLEX
-  TetGenWorker worker( connection );
-  worker.meshJob();
-  return 0;
-}
